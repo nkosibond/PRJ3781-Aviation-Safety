0e3529b57ebddf46b45ef8d03d459e3b0408d3ff
Nkosinathi Clive Ndlovu <nkosinathiclivendlovu@gmail.com>
2024-10-21 14:13:29 +0200

    Add files via upload

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Aviation
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
